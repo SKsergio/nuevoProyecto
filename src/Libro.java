55cae81f9efd888337e2106300870731feecd162
SKsergio <sergiofloress911@gmail.com>
2026-03-09 19:46:31 -0600

    asignacion de tareas

---
+++ src/Libro.java
@@ -0,0 +1,11 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String ibsm;
+    private int numeroPaginas;
+
+    //constructor(cRISTIAN)
+
+
+    //GETTERS Y SETTERS(Adriana)
+}
